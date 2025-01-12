b888a1333d920584889113dd9464596d2d9f7d1e
Richard Nespithal <richard@nespithal.com>
2025-01-12 21:16:00 +0100

    chore: Add action support

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # Import StreamController modules
 from src.backend.PluginManager.PluginBase import PluginBase
 from src.backend.PluginManager.ActionHolder import ActionHolder
+from src.backend.PluginManager.ActionInputSupport import ActionInputSupport
+from src.backend.DeckManagement.InputIdentifier import Input
 
 # Import actions
 from .actions.ElgatoKeyLight.SetButton import SetButton
 from .actions.ElgatoKeyLight.ToggleButton import ToggleButton
-
 from .actions.ElgatoKeyLight.Dial import Dial
 
 class PluginTemplate(PluginBase):
@@ -18,6 +19,7 @@ class PluginTemplate(PluginBase):
             action_base = SetButton,
             action_id = "com_memclash_elgatokeylight::SetButton", # Change this to your own plugin id
             action_name = "Set Light Properties",
+            action_support={Input.Key: ActionInputSupport.SUPPORTED},
         )
 
         self.toggle_light_action_holder = ActionHolder(
@@ -25,6 +27,7 @@ class PluginTemplate(PluginBase):
             action_base = ToggleButton,
             action_id = "com_memclash_elgatokeylight::ToggleButton", # Change this to your own plugin id
             action_name = "Toggle Light",
+            action_support={Input.Key: ActionInputSupport.SUPPORTED, Input.Dial: ActionInputSupport.SUPPORTED},
         )
 
         self.dial_action_holder = ActionHolder(
@@ -32,6 +35,7 @@ class PluginTemplate(PluginBase):
             action_base = Dial,
             action_id = "com_memclash_elgatokeylight::Dial", # Change this to your own plugin id
             action_name = "Dial Brightness Temperature",
+            action_support={Input.Dial: ActionInputSupport.SUPPORTED, Input.Key: ActionInputSupport.UNSUPPORTED},
         )
 
         self.add_action_holder(self.set_light_action_holder)
