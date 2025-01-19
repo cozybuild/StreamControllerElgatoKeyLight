10dc780a80dd58098dfc9fe44ce2bc44723a112a
Richard Nespithal <richard@nespithal.com>
2025-01-19 12:32:01 +0100

    feat: New button for inc/decrease light properties
    Closes #3

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from src.backend.DeckManagement.InputIdentifier import Input
 
 # Import actions
 from .actions.ElgatoKeyLight.SetButton import SetButton
+from .actions.ElgatoKeyLight.IncreaseDecreaseButton import IncreaseDecreaseButton
 from .actions.ElgatoKeyLight.ToggleButton import ToggleButton
 from .actions.ElgatoKeyLight.Dial import Dial
 
@@ -28,6 +29,14 @@ class PluginTemplate(PluginBase):
             action_support={Input.Key: ActionInputSupport.SUPPORTED},
         )
 
+        self.increase_decrease_light_action_holder = ActionHolder(
+            plugin_base = self,
+            action_base = IncreaseDecreaseButton,
+            action_id = "com_memclash_elgatokeylight::IncreaseDecreaseButton", # Change this to your own plugin id
+            action_name = "Increase/Decrease Light Properties",
+            action_support={Input.Key: ActionInputSupport.SUPPORTED},
+        )
+
         self.toggle_light_action_holder = ActionHolder(
             plugin_base = self,
             action_base = ToggleButton,
@@ -45,6 +54,7 @@ class PluginTemplate(PluginBase):
         )
 
         self.add_action_holder(self.set_light_action_holder)
+        self.add_action_holder(self.increase_decrease_light_action_holder)
         self.add_action_holder(self.toggle_light_action_holder)
         self.add_action_holder(self.dial_action_holder)
 
